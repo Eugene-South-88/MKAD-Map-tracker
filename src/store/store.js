487491c25e9f1d3cab9d590d985b981da23d999d
Eugene <me.eugene.south@gmail.com>
2025-04-04 13:23:51 +0300

    "Настроил деплой Vue на GitHub Pages"

--- src/store/store.js
+++ src/store/store.js
@@ -3,13 +3,11 @@ import { defineStore } from 'pinia';
 export const useStore = defineStore('store', {
   state: () => ({
     isLoading: false,
-    locationDetails: localStorage.getItem('locationDetails') || '', // Восстановление значения из localStorage
+    locationDetails: '', // Восстановление значения из localStorage
   }),
   actions: {
     defineInfoText(text) {
       this.locationDetails = text;
-      // Сохраняем locationDetails в localStorage
-      localStorage.setItem('locationDetails', text);
     },
   },
 });
